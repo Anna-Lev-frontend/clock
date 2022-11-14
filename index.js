1c8e2a00e5fa4fcf18292e22d4a5b84a090b46c6
Anna Levkovich <7604442@gmail.com>
2022-11-15 00:15:07 +0300

    подключила date

--- index.js
+++ index.js
@@ -1,19 +1,11 @@
-// const clock = setInterval((
-// ) => {
-//     const date = new Date();
-//     const min = date.getMinutes();
-//     const sec = date.getSeconds();
-//     const hour = date.getHours();
-//     console.log(hour, min, sec);
-// }, 1000);
+
 
 const makePointsDeal = () => {
     const points = new Array(12).fill(null);
     const arrayDiv = points.map((value, index) => {
         const div = document.createElement('div');
-        const xPosition
-        const yPosition
-        div.style.transform = `translate(${xPosition}px,${yPosition}px)`;
+      
+       div.style = `transform: translate(0px, -200px) rotate(${(index + 1) * 30}deg); transform-origin: center 235px;`
 
         div.classList.add('point');
         div.innerText = index + 1;
@@ -22,14 +14,45 @@ const makePointsDeal = () => {
     return arrayDiv
 }
 
+const makeArrows = () => {
+    const makeArrows = document.createElement('div');
+    makeArrows.classList.add('make-arrows')
+    const arrowHour = document.createElement('div');
+    arrowHour.classList.add('hr');
+        
+    const arrowMin = document.createElement('div');
+    arrowMin.classList.add('mn');
+  
+    const arrowSec = document.createElement('div');
+    arrowSec.classList.add('sc');
+    makeArrows.append(arrowHour,arrowMin,arrowSec);
+
+    return makeArrows;
+}
+
 const makeDial = () => {
     const mainDial = document.createElement('div');
     mainDial.classList.add('main-dial');
     const pointsDeal = makePointsDeal();
-    mainDial.append(...pointsDeal)
+    const arrow = makeArrows();
+    mainDial.append(...pointsDeal, arrow)
+
     return mainDial;
 }
-document.body.append(makeDial())
+const clock = setInterval((
+) => {
+    const deg = 6;
+    const date = new Date();
+    const min = date.getMinutes() * deg;
+    const sec = date.getSeconds() * deg;
+    const hour = date.getHours() * 30;
+    const hr = document.querySelector('.hr');
+    const sc = document.querySelector('.sc');
+    const mn = document.querySelector('.mn')
+    hr.style.transform = `rotateZ(${(hour) + (min / 12)}deg)`;
+    mn.style.transform = `rotateZ(${min}deg)`;
+     sc.style.transform = `rotateZ(${sec}deg)`;
 
-// [null,null,null]
-// [div,div,div]
+},);
+
+document.body.append(makeDial())
